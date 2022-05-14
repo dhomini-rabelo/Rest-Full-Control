19381953b9d4191c0996fe70f294bda62f6fa4d1
dhomini-rabelo <dhominisalgado@gmail.com>
2022-05-14 19:22:29 -0300

    add admin for models in sales app

--- project/backend/sales/app/admin.py
+++ project/backend/sales/app/admin.py
@@ -1,18 +1,31 @@
 from django.contrib import admin
-# from .models import Sale
-# admin.site.empty_value_display = 'NULL'
+from .models import Sale, PaymentType, FreightCompany
 
 
+admin.site.empty_value_display = 'NULL'
 
 
-# @admin.register(Sale)
-# class SaleAdmin(admin.ModelAdmin):
-#     list_display = '',
-#     list_display_links = '',
-#     list_filter = '',
-#     list_per_page = 50
-#     list_select_related = False # use tuple, default is False
-#     ordering = '',
-#     actions = None
-#     prepopulated_fields = {'slug': 'title',}
-#     search_fields = '', # ^ -> startswith, = -> iexact, @ ->	search, None -> icontains
+@admin.register(PaymentType)
+class PaymentTypeAdmin(admin.ModelAdmin):
+    list_display = 'id', 'name',
+    list_display_links = 'name',
+    list_per_page = 50
+    ordering = 'name',
+    search_fields = '^name',
+
+@admin.register(FreightCompany)
+class FreightCompanyAdmin(admin.ModelAdmin):
+    list_display = 'id', 'name',
+    list_display_links = 'name',
+    list_per_page = 50
+    ordering = 'name',
+    search_fields = '^name',
+
+@admin.register(Sale)
+class SaleAdmin(admin.ModelAdmin):
+    list_display = 'id', 'sale_price', 'sale_date'
+    list_display_links = 'id',
+    list_per_page = 50
+    list_select_related = 'user', 'product', 'freight_company', 'payment_type'
+    ordering = 'sale_date',
+    search_fields = 'sale_date',
