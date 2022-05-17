7cea4bbc84a0ef3c63947aadf88c3442ffd63d56
dhomini-rabelo <dhominisalgado@gmail.com>
2022-05-17 19:42:05 -0300

    adding models to filter and selector class

--- project/backend/products/actions/objects/data_control/products/filter.py
+++ project/backend/products/actions/objects/data_control/products/filter.py
@@ -10,10 +10,50 @@ product_filter = FilterForQueryset({
     'not_has_promotion': 'promotion_price__isnull',
     'cashback_value_gte': 'cashback_value__gte',
     'cashback_value_lte': 'cashback_value__lte',
+    'cashback_is_percent': 'cashback_is_percent',
     'not_has_cashback': 'cashback_value__isnull',
     'company': 'company__name__startswith',
     'category': 'categories__name__iexact',
     'coupon': 'coupons__name__iexact',
     'feedbacks': 'feedbacks__user__id',
     'ratings': 'ratings__user__id',
+}, models = {
+    "promotion": [
+        {
+            "not_has_promotion": False
+        }
+    ],
+    "cashback": [
+        {
+            "not_has_cashback": False
+        }
+    ],
+    "promotion_or_cashback": [
+        {
+            "not_has_promotion": False
+        },
+        {
+            "not_has_cashback": False
+        }
+    ],
+    "promotion_and_cashback": [
+        {
+            "not_has_promotion": False,
+            "not_has_cashback": False
+        }
+    ],
+    "price_gte_3000": [
+        {
+            "price_gte": 3000
+        },
+        {
+            "promotion_price_gte": 3000
+        }
+    ],
+    "cashback_gte_10": [
+        {
+            "cashback_value_gte": 10,
+            "cashback_is_percent": True
+        }
+    ]
 })
--- project/backend/products/actions/objects/data_control/products/selector.py
+++ project/backend/products/actions/objects/data_control/products/selector.py
@@ -2,9 +2,58 @@ from Core.views.data_control.handlers.selector import SelectorForQueryset
 
 
 product_selector = SelectorForQueryset({
-    "name": {
-        "simple_fields": [
-            "name"
-        ]
-    }
+    "list": {
+        "fields": [
+            "name",
+            "current_price",
+            "promotion_price",
+            "cashback_value",
+            "cashback_is_percent",
+            "ratings",
+            "categories",
+        ],
+        "relationship_fields": {
+            "ratings": [
+                "value",
+            ],
+            "categories": [
+                "name"
+            ]
+        }
+    },
+    "details": {
+        "fields": [
+            "name",
+            "description",
+            "current_price",
+            "promotion_price",
+            "cashback_value",
+            "cashback_is_percent",
+            "company",
+            "feedbacks",
+            "ratings"
+        ],
+        "relationship_fields": {
+            "ratings": [
+                "value",
+            ]
+        }
+    },
+    "on_sale": {
+        "fields": [
+            "name",
+            "current_price",
+            "promotion_price",
+            "cashback_value",
+            "cashback_is_percent",
+            "coupons"
+        ],
+        "relationship_fields": {
+            "coupons": [
+                "name",
+                "value",
+                "is_percent"
+            ]
+        }
+    },
 })
