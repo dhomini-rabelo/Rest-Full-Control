821313c3d53882541fb2efd7951ce333955d28a9
dhomini-rabelo <dhominisalgado@gmail.com>
2022-05-19 09:57:28 -0300

    updating view

--- project/Core/views/data_control/view.py
+++ project/Core/views/data_control/view.py
@@ -16,22 +16,25 @@ class DataControlApi(APIView, ABC):
     Require variables:
         serializer_class ( ModelSerializer ) : The default serializer class
         initial_queryset ( Queryset ) : The default queryset
-        filter_function ( Function(queryset: QuerySet, body: dict) -> QuerySet ) : This function filter initial_queryset based in request.body
-        selector_function ( Function(queryset: QuerySet, serializer: ModelSerializer, body: dict) -> ModelSerializer.data | ListSerializer.data ) : This function
+        filter_function ( Function(self, queryset: QuerySet, body: dict) -> QuerySet ) : This function filter initial_queryset based in request.body
+        selector_function ( Function(self, queryset: QuerySet, serializer: ModelSerializer, body: dict) -> ModelSerializer.data | ListSerializer.data ) : This function
         select fields and returns response based in request.body
     """
 
     def get(self, request) -> ReturnDict | ReturnList:
-        body = request.data if request.data and isinstance(request.data, dict) else {}
-        initial_queryset = self.get_queryset()
-
         try:
+            body = self._get_body(request)
+            initial_queryset = self.get_queryset()
             new_queryset = self.filter_function(initial_queryset, body)
             response = self.selector_function(new_queryset, self.get_serializer_class(), body)
         except Exception as error:
             return Response({get_type_name(error): str(error)}, status=status.HTTP_400_BAD_REQUEST)
-
         return Response(response)
+
+    def _get_body(self, request):
+        body = request.data or {}
+        if not isinstance(body, dict): raise TypeError('Body must be a dict')
+        return body
 
     def get_queryset(self) -> QuerySet:
         return self.initial_queryset
