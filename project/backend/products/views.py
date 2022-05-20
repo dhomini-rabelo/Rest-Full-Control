ce754916bfebcbea11170a98aec581703b7c4d61
Dhomini  Rabelo <dhominisalgado@gmail.com>
2022-05-20 01:50:56 -0300

    updating simple

--- project/backend/products/views.py
+++ project/backend/products/views.py
@@ -8,6 +8,6 @@ from .actions.objects.data_control.products.selector import product_selector
 
 class ProductDataControl(DataControlApi):
     serializer_class = ProductSerializer
-    initial_queryset = Product.objects.select_related('company').prefetch_related('categories', 'coupons', 'feedbacks', 'ratings')
+    initial_queryset = Product.objects.all().select_related('company').prefetch_related('categories', 'coupons', 'feedbacks', 'ratings')
     filter_function = product_filter.filter_queryset
     selector_function = product_selector.select
